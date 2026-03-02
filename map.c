5efb9a23421592741f9963747b5af35d90065456
wito <witek.waligora@gmail.com>
2026-03-02 22:01:56 +0100

    start

--- map.c
+++ map.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct{
+	char*** array; //array of arrays of strings
+	int size;
+} Map;
 
+void init_map(Map* map){
+	map->size = 0;
+	map->array = malloc(sizeof(char)*20000*100); 
+}
 
+int push_back(Map* map, char elem){
+
+}
 
 int main(){
 }
